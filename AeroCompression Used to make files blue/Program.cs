db45e2139aa2208d62279856500f5c29b8a36f18
Nicholas Halka <88tontos@gmail.com>
2018-10-18 01:53:24 -0400

    Earliest Initial Code Commit

---
+++ AeroCompression Used to make files blue/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace AeroCompression
+{
+    class Program
+    {
+        [DllImport("kernel32.dll")]
+        public static extern int DeviceIoControl(IntPtr hDevice, int
+            dwIoControlCode, ref short lpInBuffer, int nInBufferSize, IntPtr
+        lpOutBuffer, int nOutBufferSize, ref int lpBytesReturned, IntPtr
+            lpOverlapped);
+
+        static void Main(string[] args)
+        {
+
+            string fileName = @"C:\Users\Nicholas\Desktop\Minecraft Technology CheckMC By Aeroverra\AeroMC Final\UFA2.txt";
+            int lpBytesReturned = 0;
+            int FSCTL_SET_COMPRESSION = 0x9C040;
+            short COMPRESSION_FORMAT_DEFAULT = 1;
+
+            FileStream f = File.Open(fileName, System.IO.FileMode.Open,
+              System.IO.FileAccess.ReadWrite, System.IO.FileShare.None);
+
+
+            int result = DeviceIoControl(f.Handle, FSCTL_SET_COMPRESSION,
+              ref COMPRESSION_FORMAT_DEFAULT, 2 /*sizeof(short)*/, IntPtr.Zero, 0,
+              ref lpBytesReturned, IntPtr.Zero);
+        }
+    }
+}
+
